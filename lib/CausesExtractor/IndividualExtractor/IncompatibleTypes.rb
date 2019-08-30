3f8952244428fcfb621493b789e4e63106cabe1e
matheus <mbmt@cin.ufpe.br>
2019-08-30 14:08:19 -0300

    added IncompatibleTypes

---
+++ lib/CausesExtractor/IndividualExtractor/IncompatibleTypes.rb
@@ -0,0 +1,48 @@
+class IncompatibleTypes
+
+	def initialize()
+		
+	end
+
+	def extractionFilesInfo(buildLog)
+		filesInformation = []
+		logs = buildLog.to_enum(:scan, /^[\--z]+:\d+: error: incompatible types: \w+ cannot be converted to \w+$/).map { Regexp.last_match }
+		begin
+			if (buildLog[/^[\--z]+:\d+: error: incompatible types: \w+ cannot be converted to \w+$/])
+				count = 0
+				while (count < logs.size)
+					line = logs[count].to_s[/\.java:\d+/].split(":").last
+					classFile = logs[count].to_s[/[\/\-\_\w]+\.java:/].split("/").last.gsub(".java:","")
+					typeAndExpected = logs[count].to_s[/\w+ cannot be converted to \w+/].to_s.split(" ")
+					type = typeAndExpected[0]
+					expectedType = typeAndExpected[5]
+					filesInformation.push(["IncompatibleTypes",classFile, type, expectedType,line])
+					count += 1
+				end
+			end		
+			return "IncompatibleTypes", filesInformation, logs.size
+		rescue
+			return "IncompatibleTypes", [], 0
+		end
+	end
+
+end
+
+str = ""
+
+File.open("test.txt").each do |line| 
+	str = str + line
+end
+
+extractor = IncompatibleTypes.new()
+
+puts "=============================="
+puts "####about to extract info#####"
+puts "=============================="
+info = extractor.extractionFilesInfo(str)
+puts "=============================="
+puts "########info extracted########"
+puts "=============================="
+
+puts "Files information: "
+puts info
