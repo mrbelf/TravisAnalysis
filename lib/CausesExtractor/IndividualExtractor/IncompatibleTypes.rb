12d452d2841dcc4b2d8c14b396af058478aea6e2
matheus <mbmt@cin.ufpe.br>
2019-09-05 14:51:53 -0300

    added method to find the name of the variable and method causing the error

--- lib/CausesExtractor/IndividualExtractor/IncompatibleTypes.rb
+++ lib/CausesExtractor/IndividualExtractor/IncompatibleTypes.rb
@@ -17,7 +17,7 @@ class IncompatibleTypes
 						typeAndExpected = logs[count].to_s[/\w+ cannot be converted to \w+/].to_s.split(" ")
 						type = typeAndExpected[0]
 						expectedType = typeAndExpected[5]
-						filesInformation.push(["IncompatibleTypes",classFile, type, expectedType,line])
+						filesInformation.push(["IncompatibleTypes",classFile, type, expectedType,line.to_i])
 					else
 						line = logs[count].to_s[/\.java:\[\d+/].split("[")[1]
 						classFile = logs[count].to_s[/[\/\-\_\w]+\.java:/].split("/").last.gsub(".java:","")
@@ -25,7 +25,7 @@ class IncompatibleTypes
 						type = typeAndExpected[0]
 						expectedType = typeAndExpected[5]
 						column = logs[count].to_s[/\.java:\[\d+,\d+/].split(",")[1]
-						filesInformation.push(["IncompatibleTypes",classFile, type, expectedType,line,column])
+						filesInformation.push(["IncompatibleTypes",classFile, type, expectedType,line.to_i,column.to_i])
 					end
 
 					count += 1
@@ -35,6 +35,30 @@ class IncompatibleTypes
 		rescue
 			return "IncompatibleTypes", [], 0
 		end
+	end
+
+	#considerando uma única String como entrada, e com informação de coluna
+	def findVariableMethod (erroredLine,column, javaClass)
+		count = 0
+		var = ""
+		changedMethod = ""
+		javaClass.each_line do |line|
+			count += 1
+			if(count == erroredLine)
+				line = line.split(".")
+				var = line[line.size-2].gsub(" ","").gsub("	","")
+				changedMethod = ""
+				line[line.size-1].each_char do |char|
+					if(char != '(')
+						changedMethod = changedMethod + char
+					else
+						break
+					end
+				end
+			end
+			
+		end
+		return [var,changedMethod]
 	end
 
 end
