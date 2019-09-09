fc21b6524b36156721a2d9f0b84a57a64691631b
matheus <mbmt@cin.ufpe.br>
2019-09-09 13:51:59 -0300

    added methods to extract more information from the error

--- lib/CausesExtractor/IndividualExtractor/IncompatibleTypes.rb
+++ lib/CausesExtractor/IndividualExtractor/IncompatibleTypes.rb
@@ -37,28 +37,116 @@ class IncompatibleTypes
 		end
 	end
 
-	#considerando uma única String como entrada, e com informação de coluna
-	def findVariableMethod (erroredLine,column, javaClass)
-		count = 0
+	def findVariable (erroredLine,column, javaClass)
 		var = ""
 		changedMethod = ""
-		javaClass.each_line do |line|
-			count += 1
-			if(count == erroredLine)
-				line = line.split(".")
-				var = line[line.size-2].gsub(" ","").gsub("	","")
-				changedMethod = ""
-				line[line.size-1].each_char do |char|
-					if(char != '(')
-						changedMethod = changedMethod + char
-					else
-						break
-					end
-				end
-			end
-			
-		end
-		return [var,changedMethod]
+		javaClass = javaClass.split("\n")
+		line = javaClass[erroredLine-1].split(".")
+		var = line[line.size-2].gsub(" ","").gsub("	","")
+		return var	
 	end
 
+	def thisWasUsed (erroredLine, column, javaClass, var)
+		return !javaClass.split("\n")[erroredLine-1][/this\.#{var}\.\w+/].nil?
+	end
+
+	def removeComments(javaClass)
+		finalJavaClass = ""
+		commentedLine = false
+		commentedBlock = false
+		singleSlash = false
+		singleAsterisk = false
+		javaClass.each_char do |char|
+			if(char == "/" || char == "*")
+				if(char == "*" && singleSlash)
+					singleSlash = false
+					commentedBlock = true
+				elsif(char == "/" && singleAsterisk)
+					singleAsterisk = false
+					commentedBlock = false
+					next
+				elsif(char == "/" && singleSlash)
+					singleSlash = false
+					commentedLine = true
+				elsif(char == "*")
+					singleAsterisk = true
+					next
+				else
+					singleSlash = true
+					next
+				end
+			else
+				if(!commentedLine && !commentedBlock)
+					if(singleSlash)
+						finalJavaClass = finalJavaClass + "/"
+					elsif(commentedLine)
+						finalJavaClass = finalJavaClass + "*"
+					end
+				end
+				singleSlash = false
+				singleAsterisk = false
+			end
+		
+			if(commentedLine)
+				if(char == "\n")
+					finalJavaClass = finalJavaClass + char
+					commentedLine = false
+				else
+					next
+				end
+			elsif(commentedBlock)
+				if(char == "\n")
+					finalJavaClass = finalJavaClass + char
+				else
+					next
+				end	
+			else
+				finalJavaClass = finalJavaClass + char
+			end		
+		end
+		return finalJavaClass
+	end
+
+	def findClass(javaClass)
+		javaClass = removeComments(javaClass)
+		returnLine = -1
+		count = 0
+		found = false
+		javaClass.each_line do |line|
+			if(line[/public[\ ]+class[\ ]+\w+ [\w0-9\ \_\-]+{/] || line[/private[\ ]+class[\ ]+\w+ [\w0-9\ \_\-]+{/] || line[/protected[\ ]+class[\ ]+\w+ [\w0-9\ \_\-]+{/])
+				if(found)
+					return [-1,false]
+				else
+					returnLine = count
+					found = true
+				end
+			end
+			count += 1
+		end
+		return [returnLine,true]
+	end
+
+	def findAttributeType(javaClass,classStartsAt,var,erroredLine, thisWasUsed)
+		javaClass = javaClass.split("\n")
+		found = false
+		type = ""
+		count = classStartsAt+1
+		begin
+			while(!found)
+				if(javaClass[count].include?("{") || javaClass[count].include?("}"))
+					return ["",false]
+				end
+				if(count >= erroredLine)
+					return ["",false]
+				end
+				if(!javaClass[count][/private[\ ]+\w+[\ ]+#{var}/].nil?)
+					return [javaClass[count].split(" ")[1],true&&thisWasUsed]
+				end
+			count += 1
+			end
+			return ["",false]
+		rescue
+			return ["",false] 
+		end
+	end
 end
