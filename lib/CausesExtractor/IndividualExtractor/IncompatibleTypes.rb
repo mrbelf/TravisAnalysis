f685338f2c78d9a7d21c0a95d1f2f9d1069cfb8f
matheus <mbmt@cin.ufpe.br>
2019-09-09 17:06:57 -0300

    instavel, adicionada leitura de arquivo do repositorio

--- lib/CausesExtractor/IndividualExtractor/IncompatibleTypes.rb
+++ lib/CausesExtractor/IndividualExtractor/IncompatibleTypes.rb
@@ -4,28 +4,76 @@ class IncompatibleTypes
 		
 	end
 
-	def extractionFilesInfo(buildLog)
+	def extractionFilesInfo(buildLog, pathProject)
+		pathAtual = Dir.pwd
+		projectName = pathProject.split("/").last
+		Dir.chdir pathProject
 		filesInformation = []
 		logs = buildLog.to_enum(:scan, /^[\--z ]+:[\[]?[\d\,]+[\]]?[\:]? error: incompatible types: \w+ cannot be converted to \w+$/).map { Regexp.last_match }
 		begin
 			if (buildLog[/^[\--z ]+:[\[]?[\d\,]+[\]]?[\:]? error: incompatible types: \w+ cannot be converted to \w+$/])
 				count = 0
 				while (count < logs.size)
-					if(logs[count].to_s[/\[/].nil?)					
+					if(logs[count].to_s[/\[/].nil?)			
 						line = logs[count].to_s[/\.java:\d+/].split(":").last
 						classFile = logs[count].to_s[/[\/\-\_\w]+\.java:/].split("/").last.gsub(".java:","")
 						typeAndExpected = logs[count].to_s[/\w+ cannot be converted to \w+/].to_s.split(" ")
 						type = typeAndExpected[0]
 						expectedType = typeAndExpected[5]
-						filesInformation.push(["IncompatibleTypes",classFile, type, expectedType,line.to_i])
-					else
+						javaFile = classFile + ".java"
+						files = %x(find -name #{javaFile})
+												
+						erroredClass = findAttributeType(removeComments(javaClass), findClass(javaClass)[0], findVariable(line,-1,javaClass), line, thisWasUsed(line,-1,javaClass, it.findVariable(line,-1,javaClass)))
+
+						filesInformation.push(["IncompatibleTypes",classFile, type, expectedType,line.to_i, erroredClass])
+					else	
 						line = logs[count].to_s[/\.java:\[\d+/].split("[")[1]
 						classFile = logs[count].to_s[/[\/\-\_\w]+\.java:/].split("/").last.gsub(".java:","")
 						typeAndExpected = logs[count].to_s[/\w+ cannot be converted to \w+/].to_s.split(" ")
 						type = typeAndExpected[0]
 						expectedType = typeAndExpected[5]
 						column = logs[count].to_s[/\.java:\[\d+,\d+/].split(",")[1]
-						filesInformation.push(["IncompatibleTypes",classFile, type, expectedType,line.to_i,column.to_i])
+						javaFile = classFile + ".java"
+						files = %x(find -name #{javaFile})
+						finalFile = ""
+						files.each_line do |file|
+							pathInProject = ""
+							log = logs[count].to_s[/#{projectName}[\/\w\-]+\.java/][0].to_s.split("/")
+							i = 1
+							log.each do |str|
+								if(log.size != i)
+									pathInProject = pathInProject + str + "/"
+								end
+								i += 1
+							end
+							if(file.include?(pathInProject))
+								finalFile = file
+								break
+							end
+						end
+
+						finalDir = ""
+						i = 1
+						finalFile.split("/").each do |str|
+							if(i != finalFile.split("/").size)
+								finalDir = finalDir + str + "/"
+							end 
+						i += 1
+						end
+						Dir.chdir finalDir
+						javaClass = ""
+						puts finalDir
+						File.open(javaFile).each do |line|
+							javaClass = javaClass + line
+						end
+						javaClass = removeComments(javaClass)
+						classStartsAt = findClass(javaClass)
+						variableName = findVariable(line,column,javaClass)
+						puts "got here" 
+						puts classStartsAt[0]
+						erroredClass = findAttributeType(javaClass, classStartsAt[0].to_i, variableName, line.to_i, thisWasUsed)
+						puts erroredClass
+						filesInformation.push(["IncompatibleTypes",classFile, type, expectedType,line.to_i,column.to_i,erroredClass])
 					end
 
 					count += 1
@@ -38,6 +86,7 @@ class IncompatibleTypes
 	end
 
 	def findVariable (erroredLine,column, javaClass)
+		erroredLine = erroredLine.to_i
 		var = ""
 		changedMethod = ""
 		javaClass = javaClass.split("\n")
@@ -108,7 +157,6 @@ class IncompatibleTypes
 	end
 
 	def findClass(javaClass)
-		javaClass = removeComments(javaClass)
 		returnLine = -1
 		count = 0
 		found = false
@@ -146,7 +194,20 @@ class IncompatibleTypes
 			end
 			return ["",false]
 		rescue
-			return ["",false] 
+			return ["",false]
 		end
 	end
 end
+
+
+
+
+test = ""
+File.open("test.txt").each do |line|
+	test = test + line
+end
+
+it = IncompatibleTypes.new()
+
+info1 = it.extractionFilesInfo(test, "/home/ines/Documents/neo4j-timetree")
+puts info1
